e83a4d79787a401bc4ee75d496908058ed7db12a
Thawan Barroso <thawancomt@gmail.com>
2024-01-18 22:56:25 +0000

    update gitignore

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'media',
-  content: ["System/templates/*/*.{html,js}", "System/templates/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
