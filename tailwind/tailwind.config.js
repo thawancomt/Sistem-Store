db4b71e1f9bf4c851dddaa94d7f553c051e99d83
Thawan Barroso <thawancomt@gmail.com>
2024-01-18 23:04:02 +0000

    update gitattributes

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: ["System/templates/*/*.{html,js}", "System/templates/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
