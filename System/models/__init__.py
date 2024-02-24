89628412a774018ff053b6e70c58ab46f460b2f4
Thawan Barroso <thawancomt@gmail.com>
2024-02-24 12:09:45 +0000

    Refactoring the project structure

--- System/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .stock import StockArticles, StoreStock
-from .production import Production, Consumes, Wasted
-from .users import CreateUser, Login, User, Session
-from .stores_management import Store
-from .utils import *
