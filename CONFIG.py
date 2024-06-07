e0109891267f0d438fd4ced8eae8939e0f6908e9
Thawan Barroso <thawancomt@gmail.com>
2024-06-07 11:27:05 +0100

    Change to sqlite database

--- CONFIG.py
+++ CONFIG.py
@@ -4,5 +4,5 @@ PORT = 5002
 
 # Set the sqlachemy database URI
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1633@localhost:3306/storedb'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
