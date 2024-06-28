72401d72314926bc6e9cb49a9294de80be4484dd
Thawan Barroso <thawancomt@gmail.com>
2024-06-28 19:07:55 +0100

    removing the initial config

--- extensions/README.md
+++
@@ -1,3 +0,0 @@
-## for production run these codes in this folder
-1 npm install -D tailwindcss
-2 npx tailwindcss init 
--- extensions/tailwind.config.js
+++ extensions/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['../flaskr/blueprints/**/templates/*.{html,js}', '../flaskr/templates/**/*.{html,js}'],
+  content: ['../store/blueprints/**/templates/*.{html,js}', '../store/templates/**/*.{html,js}'],
   theme: {
     extend: {},
     darkMode : 'media'
