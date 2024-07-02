1fbce55d2a9563831db371b70a876442605f0ae4
Thawan Barroso <thawancomt@gmail.com>
2024-07-02 01:22:33 +0100

    Animated chartJs

--- store/blueprints/production/static/chart.js
+++ store/blueprints/production/static/chart.js
@@ -6,31 +6,46 @@ const selectLimit = document.getElementById('limit')
 
 const newLine = Array.from(dataset)
 
-
-
+// Clonando os arrays originais
+const originalLabels = Array.from(dataset.labels);
+const originalDatasets = dataset.datasets.map(data => ({
+    ...data,
+    data: Array.from(data.data)
+}));
 
 const productionChart = new Chart(ctx, {
     type: 'line',
-    data: {
-        labels: labels,
-        datasets: dataset,
-    },
+    data: dataset,
     options: {
-        
-        maintainAspectRatio : false,
+        maintainAspectRatio: false,
         responsive: true
     },
-})
+});
 
+// Extensão do protótipo do Chart para limitar os dias
 Chart.prototype.limitDays = function(days) {
-    if (days > this.data.labels.length) {
-      this.data.labels = Array.from(productionLabels)
+    // Verificar se o número de dias solicitado é maior do que o número total de labels
+    if (days > originalLabels.length) {
+        this.data.labels = Array.from(originalLabels);
+        // this.data.datasets.forEach((dataset, index) => {
+        //     dataset.data = Array.from(originalDatasets[index].data);
+        // });
+    } else {
+        // Fatiar os labels para manter apenas os últimos 'days' labels
+        this.data.labels = originalLabels.slice(-days);
+        
+        // Fatiar os dados em cada dataset para corresponder aos labels
+        this.data.datasets.forEach((dataset, index) => {
+            dataset.data = originalDatasets[index].data.slice(-days);
+        });
     }
-      this.data.labels = this.data.labels.splice(-days)
-    this.update()
-  }
 
+    // Atualizar o gráfico para refletir as mudanças
+    
+}
 
+// Event listener para alterar o número de dias visualizados
 selectLimit.addEventListener('change', () => {
-    productionChart.limitDays(selectLimit.value)
-})
+    productionChart.limitDays(selectLimit.value);
+    productionChart.update();
+});
