88add76d0d56a938fa27f8517dedc0e6aeafa583
Thawan Barroso <thawancomt@gmail.com>
2024-08-27 18:31:46 +0100

    HotFix 6 on create order page and new features
    Now we have a button to view the order content on review order page.
    Initial of work on daily task views to follow the news project standards

--- store/blueprints/daily_tasks/views/DailyView.py
+++ store/blueprints/daily_tasks/views/DailyView.py
@@ -73,3 +73,74 @@ def edit_task():
     return redirect(url_for('daily_tasks.index'))
 
 
+class DailyTasksBlueprint:
+    def __init__(self, name='daily_tasks', import_name=__name__,
+                 template_folder='../templates',
+                 url_prefix='/daily_tasks',
+                 static_folder='../static'):
+        
+        self.blueprint = Blueprint(name, import_name,
+                                template_folder=template_folder,
+                                static_folder=static_folder,
+                                url_prefix=url_prefix)
+        
+    #@daily_tasks.route('/')
+    def index():
+        context = {
+            'active_daily_tasks' : DailyTasksService().get_all_active_tasks(),
+            'to_do' : DailyStatusService(date=g.date).get_all_tasks(),
+        }
+        
+        return render_template('daily_tasks.html', context = context)
+
+    #@daily_tasks.route('/create', methods=['POST'])
+    @fresh_login_required
+    def create():
+        data = request.form.to_dict()
+        DailyTasksService().create_task(data)
+        return redirect('/daily_tasks')
+
+    # Create the set as done method
+    #@daily_tasks.route('/finish', methods=['POST'])
+    @login_required
+    def set_as_done():
+        data = request.form.to_dict()
+        
+        day_status = DailyStatusService(date=g.date)
+        day_status.update_day_status(data)
+        
+        return redirect(url_for('daily_tasks.index', date=g.date))
+
+    #@daily_tasks.route('/deactive', methods=['POST'])
+    @fresh_login_required
+    def deactive_task():
+        task_id = request.form.get('task_id')
+        date = request.args.get('date', g.date)
+        DailyTasksService(date=date).deactive_task(task_id)
+        return redirect('/daily_tasks')
+
+    #@daily_tasks.route('/edit', methods=['GET'])
+    @login_required
+    def edit_view():
+        task_id = request.args.get('task_id')
+        
+        task = DailyTasksService().get_task_by_id(int(task_id))
+        
+        context = {
+            'task' : task,
+        }   
+        
+        return render_template('edit_daily_task.html', context=context)
+
+    #@daily_tasks.route('/edit', methods=['POST'])
+    @login_required
+    def edit_task():
+        data = request.form.to_dict()
+        
+        if 'status' in data:
+            data['status'] = True
+        
+        DailyTasksService(data.get('task_id')).update_task(data)
+        
+        return redirect(url_for('daily_tasks.index'))
+        
